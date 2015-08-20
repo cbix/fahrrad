66e49c7cd348717ac9e3d0a9d1ff2c47c44f273b
Florian Hülsmann <fh@cbix.de>
2015-08-20 18:37:59 +0200

    ICMPv6 message dumps

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
 import (
-	"github.com/garyburd/redigo/redis"
+//	"github.com/garyburd/redigo/redis"
 	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv6"
+//	"golang.org/x/net/ipv6"
+    "fmt"
 )
+
+func main() {
+    // open listening connection
+    conn, err := icmp.ListenPacket("ip6:ipv6-icmp", "::")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer conn.Close()
+
+    // read from socket
+    err = nil
+    buf := make([]byte, 512)
+    for err == nil {
+        n, addr, err := conn.ReadFrom(buf)
+        fmt.Printf("length: %d, address: %v, type: %d, error: %v\n", n, addr, buf[0], err)
+    }
+
+}
