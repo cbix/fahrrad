0cd9d90e382117079a8c7198a8abc0e3ef2cfb95
Florian Hülsmann <fh@cbix.de>
2015-09-26 21:57:27 +0200

    redis test, package analysis

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"github.com/mediocregopher/radix.v2/redis"
+	"github.com/mediocregopher/radix.v2/redis"
 	//"github.com/mediocregopher/radix.v2/pubsub"
 	"fmt"
 	"golang.org/x/net/icmp"
@@ -14,6 +14,16 @@ const (
 )
 
 func main() {
+	// open redis connection
+	db, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//    db.Cmd("SET", append([]byte("fahrrad/test/"), []byte{0x00, 0xaa, 0xbb}...), []byte("Hello world!"))
+	//    db.Cmd("SET", append([]byte("fahrrad/test/"), []byte{0x10, 0x0a, 0xcc}...), []byte("foo bar"))
+
 	// open listening connection
 	conn, err := icmp.ListenPacket("ip6:ipv6-icmp", "::")
 	if err != nil {
@@ -25,21 +35,22 @@ func main() {
 	err = nil
 	buf := make([]byte, 512)
 	var m *icmp.Message
-	var addr net.Addr
-    var body []byte
-    var n int
+	var srcAddr net.Addr
+	var body []byte
+	var n int
 	for err == nil {
-		if n, addr, err = conn.ReadFrom(buf); err != nil {
+		if n, srcAddr, err = conn.ReadFrom(buf); err != nil {
 			continue
 		}
 		if m, err = icmp.ParseMessage(ProtocolIPv6ICMP, buf); err != nil {
 			continue
 		}
-        if body, err = m.Body.Marshal(ProtocolIPv6ICMP); err != nil {
-            continue
-        }
-
-        fmt.Printf("%v received from %v: %x\n", m.Type, addr, body[:n])
+		if body, err = m.Body.Marshal(ProtocolIPv6ICMP); err != nil {
+			continue
+		}
+		fmt.Printf("%v length %d received from %v:\n%x\n%x\n", m.Type, n, srcAddr, buf[:120], body[:120])
+        addr := srcAddr.(*net.IPAddr)
+        fmt.Printf("ip: %v\n\n", []byte(addr.IP))
 	}
-    fmt.Printf("error: %v\n", err)
+	fmt.Printf("error: %v\n", err)
 }
